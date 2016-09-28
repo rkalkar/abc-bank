45c58eba59846621da9e3915405c97091c34c19a
rkalkar <RKalkar@gmail.com>
2016-09-28 03:35:14 -0400

    Add files via upload
    added 10 days and one year dates, added transaction date

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -4,13 +4,36 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Transaction {
-    public final double amount;
 
-    private Date transactionDate;
+	public final double amount;
+	private Date transactionDate;
 
-    public Transaction(double amount) {
-        this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
-    }
+	public Transaction(double amount) {
+		this.amount = amount;
+		this.transactionDate = DateProvider.getInstance().now();
+	}
 
+	// Add getAmount and getDate for each transaction, in order to calculate
+	// interest
+	public double getAmount() {
+		return amount;
+	}
+
+	public Date getDate() {
+		return transactionDate;
+	}
+
+	public void lastYear() {
+		// Change transaction date to last year
+		// used for testing
+		long year = 24L * 3600L * 1000L * 365L;
+		transactionDate = new Date(transactionDate.getTime() - year);
+	}
+
+	public void tenDaysAgo() {
+		// Change transaction date to ten days ago
+		// used for testing
+		long tenDays = 24L * 3600L * 1000L * 10L;
+		transactionDate = new Date(transactionDate.getTime() - tenDays);
+	}
 }
